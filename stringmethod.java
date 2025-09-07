0d3f3ef4977d91c47a2e9f3ee2c174f7664a0622
JANANI GUNASEKARAN <usj592004@gmail.com>
2025-09-07 20:33:07 +0530

    Add files via upload

---
+++ stringmethod.java
@@ -0,0 +1,23 @@
+public class stringmethod {
+    public static void main(String[] args){
+
+        String name = "Bro Code";
+
+        //int length = name.length();             //shows how many letters are their
+        //char letter = name.charAt(0);           //chatAt(0) will shows the 0th index character
+        //int index = name.indexOf("o");          // indexOf("o") this will shows the index number of letter o we can also find the index of sapce [name.indexOf(" ")]
+        //int lastIndex = name.lastIndexOf("o");  // lastIndexOf("o")  this will shows the index of letter o last presence
+
+        //name = name.toUpperCase();        // change all the letter to the uppercase
+        //name = name. toLowerCase();       // change all the letter to the lowercase
+        //name = name.trim();               // remove space at first of the words(name)
+        //name = name.replace("o" , "a");   // replace the letter 'o' with the letter 'a'
+        //name = name.contains(" ");        // check if the name having any space or not like this we can also check if the name contains letter a or not [name.contains("a")]
+        //name = name.equals("janani");     //check the given name id equal to the name janani     
+        //name = name.equalsIgnoreCase("janani");  //ignore checking this case
+        
+        System.out.println(name.isEmpty()); // check if the name is empty or filled
+
+
+    }
+}
